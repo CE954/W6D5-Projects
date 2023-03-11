fa53917f0daee93f41d5be81a1f1c79e573b6b6a
chrisbanas <bananas595@gmail.com>
2023-03-10 17:05:16 -0800

    Finih partial forms

--- ninety-nine-cats/app/controllers/cats_controller.rb
+++ ninety-nine-cats/app/controllers/cats_controller.rb
@@ -1,37 +1,45 @@
 class CatsController < ApplicationController
-    def index 
+    def index
         @cats = Cat.all
         render :index
     end
 
-    def create 
+    def create
         @cat = Cat.new(cat_params)
-        if @cat.save 
+        if @cat.save
             redirect_to cat_url(@cat)
-        else  
+        else
             render json: @cat.errors.full_messages, status: 422
         end
     end
 
-    # def new 
+    def new
+        @cat = Cat.new
+        render :new
+    end
 
-    # end
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
 
-    # def edit 
-
-    # end
-
-    def show 
+    def show
         @cat = Cat.find(params[:id])
         render :show
     end
 
-    # def update
+    def update
+        @cat = Cat.find(params[:id])
+        if @cat && @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
 
-    # end
+    end
 
-    private 
-    def cat_params 
-        params.require(:cat).permit(:name, :birth_date, :color, :sex) 
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
     end
 end
